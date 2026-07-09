083cbcc7a7c859478cb11aa21a4537217bc9e2b1
Klaynight <63601267+Klaynight-dev@users.noreply.github.com>
2026-07-09 19:35:32 +0200

    fix prod

--- packages/database/prisma/repairs/20260709010000_add_verification_device_tracking.sql
+++ packages/database/prisma/repairs/20260709010000_add_verification_device_tracking.sql
@@ -1,3 +1,3 @@
-ALTER TABLE guilds ADD COLUMN "verificationSaveDevice" BOOLEAN NOT NULL DEFAULT true;
-ALTER TABLE ban_appeal_configs ADD COLUMN "appealSaveDevice" BOOLEAN NOT NULL DEFAULT true;
-ALTER TABLE security_verifications ADD COLUMN "deviceInfo" JSONB;
+ALTER TABLE guilds ADD COLUMN IF NOT EXISTS "verificationSaveDevice" BOOLEAN NOT NULL DEFAULT true;
+ALTER TABLE ban_appeal_configs ADD COLUMN IF NOT EXISTS "appealSaveDevice" BOOLEAN NOT NULL DEFAULT true;
+ALTER TABLE security_verifications ADD COLUMN IF NOT EXISTS "deviceInfo" JSONB;
